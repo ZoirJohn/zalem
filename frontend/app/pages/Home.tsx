63d0a365c73853a4cf22ff288a6f72ab28375175
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2026-04-10 15:07:53 +0500

    Enhance Home component with clipboard functionality and responsive design adjustments

--- frontend/app/pages/Home.tsx
+++ frontend/app/pages/Home.tsx
@@ -1,13 +1,25 @@
 import { Link, NavLink } from "react-router"
+import { Button } from "~/components/ui/button"
 import Star from "../assets/img/star.svg"
 import Hex from "../assets/img/hexagon.svg"
-import { Button } from "~/components/ui/button"
+import { HugeiconsIcon } from "@hugeicons/react"
+import { CopyIcon, Tick02Icon } from "@hugeicons/core-free-icons"
+import { useEffect, useState } from "react"
 
 export function meta() {
   return [{ title: "Home" }, { name: "description", content: "Homepage" }]
 }
 
 export default function Home() {
+  const [copied, setCopied] = useState(false)
+  useEffect(() => {
+    if (!copied) return
+    const timeout = setTimeout(() => setCopied(false), 1000)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [copied])
   const ConnectLink = ({
     conName,
     conLink,
@@ -20,17 +32,21 @@ export default function Home() {
         <Link
           to={conLink}
           target="_blank"
-          className="block text-center text-3xl"
+          className="block text-center sm:text-3xl text-xl"
         >
           {conName}
         </Link>
       </li>
     )
   }
+  function copy() {
+    navigator.clipboard.writeText(import.meta.env.VITE_PERSONAL_EMAIL)
+    setCopied(true)
+  }
   return (
     <section className="bg-[#72757e]">
       <div className="mx-auto grid min-h-screen max-w-4xl grid-cols-2 grid-rows-[auto_1fr_200px] gap-3 p-3">
-        <header className="component-style col-span-2 flex justify-between px-6 py-10">
+        <header className="component-style col-span-2 flex sm:justify-between justify-center gap-3 px-3 py-5 max-md:flex-wrap sm:px-6 sm:py-10">
           <h1 className="flex gap-2 uppercase">
             <span className="italic">Zoirjon</span>
             <span className="font-bold">Zokirjonov</span>
@@ -50,7 +66,7 @@ export default function Home() {
           </nav>
         </header>
 
-        <div className="component-style relative col-span-1 flex flex-col items-start justify-end p-6">
+        <div className="component-style relative col-span-2 flex flex-col items-start justify-end p-6 sm:col-span-1">
           <h1 className="text-4xl font-bold">Software Engineer</h1>
           <h1 className="text-center text-xl">Full Stack</h1>
           <img
@@ -59,11 +75,10 @@ export default function Home() {
             className="absolute top-10 right-10"
           />
         </div>
-
-        <aside className="component-style row-span-2 flex flex-col items-center justify-center gap-10">
-          <img src={Hex} alt="Hexagon icon" className="w-20" />
+        <aside className="component-style flex flex-col items-center justify-center gap-10 p-6 max-sm:col-span-2 sm:row-span-2">
+          <img src={Hex} alt="Hexagon icon" className="sm:w-20" />
           <nav>
-            <ul className="flex flex-col gap-4">
+            <ul className="flex sm:flex-col gap-4">
               {[
                 { name: "LinkedIn", link: "https://linkedin.com/in/zoirjon" },
                 { name: "GitHub", link: "https://github.com/zoirjohn" },
@@ -72,16 +87,26 @@ export default function Home() {
                   link: "https://instagram.com/zoirjonzokirjonov22",
                 },
               ].map(({ name, link }) => {
-                return <ConnectLink conName={name} conLink={link} />
+                return <ConnectLink conName={name} conLink={link} key={link} />
               })}
             </ul>
           </nav>
         </aside>
-        <div className="component-style flex items-center justify-center gap-2">
-          <p className="cursor-pointer text-xl">
-            zoirbekzokirjonov2007@gmail.com
+
+        <div className="component-style flex flex-wrap items-center justify-center gap-2 p-6 max-sm:col-span-2">
+          <p className="cursor-pointer text-lg text-wrap lg:text-xl">
+            <span className="max-md:hidden">
+              {import.meta.env.VITE_PERSONAL_EMAIL}
+            </span>
+            <span className="md:hidden">Email</span>
           </p>
-          <Button variant="secondary" className="size-8 text-xl"> </Button>
+          <Button variant="secondary" className="size-8 text-xl" onClick={copy}>
+            {copied ? (
+              <HugeiconsIcon icon={Tick02Icon} />
+            ) : (
+              <HugeiconsIcon icon={CopyIcon} />
+            )}
+          </Button>
         </div>
       </div>
     </section>
