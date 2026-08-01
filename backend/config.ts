203a4c277bc95d272d1a3324de6bb21fd695f028
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2026-08-01 18:35:18 +0500

    delete project folders, to start out fresh

--- backend/config.ts
+++
@@ -1,7 +0,0 @@
-import session from "express-session";
-import passport from "passport";
-import { sessionOptions } from "./src/utils/options";
-
-export const sessionMiddleware = session(sessionOptions);
-export const passportInitialize = passport.initialize();
-export const passportSession = passport.session();
--- backend/src/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { User as UserModel } from "../entities/user.entity";
-
-declare global {
-	namespace Express {
-		interface User extends UserModel {}
-	}
-}
-export {};
